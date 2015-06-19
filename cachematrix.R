ec0e32b369ee384bec74220dc60f6cbd0ecd6340
earlsun <earl.sun@gmail.com>
2015-06-18 21:29:41 -0500

    added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## These two functions are designed to receive a matrix, and calculate the inverse. If there is a calculation already
-## cached, it retrieves the last calculation instead of recalculating again.
+## cached, it retrieves the last calculation instead of recalculating again. These functions reside within another function.
 
 ## makeCacheMatrix receives a matrix, and NULLs the inverse cache. It has four functions: set, get, setinverse, getinverse.
+## The function does not calculate the inverse, but merely stores and retrieves whatever value the user enters.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <-NULL
@@ -18,8 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+
 ## Function checks whether there is a cached inverse value. If there is, it retrieves the stored value.
-## If not, it calculates the new inverse value of the matrix.
+## If not, it calculates the new inverse value of the matrix, and returns the calculated inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
